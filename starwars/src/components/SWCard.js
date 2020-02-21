71380c011d33129c1aad85b50566c9e4fdf3bdab
ranraok <46853842+ranraok@users.noreply.github.com>
2020-02-21 14:26:06 -0500

    added the card styling

--- starwars/src/components/SWCard.js
+++ starwars/src/components/SWCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Title from "./Title";
 import Bio from './Bio';
-  
+import styled from "styled-components";
+
+const CharCard = styled.div `
+
+  background: black;
+  color:yellow;
+  width: 200px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+
+`
+
   const SWCard = props => {
     return (
-      <div>
+      <CharCard>
          <Title name = {props.newProp.name}/>
          <Bio/>
-      </div>
+      </CharCard>
     );
   };
 
